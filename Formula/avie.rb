0685cbde2198768177501827ee548a5a133e0f14
Meet Balani <74920401+shrudge@users.noreply.github.com>
2026-04-28 14:25:24 +0530

    fix: update url to v1.0.3 and correct checksum

--- Formula/avie.rb
+++ Formula/avie.rb
@@ -1,7 +1,7 @@
 class Avie < Formula
   desc "Swift package graph diagnostics tool"
   homepage "https://github.com/shrudge/avie"
-  url "https://github.com/shrudge/avie/archive/refs/tags/v1.0.2.tar.gz"
+  url "https://github.com/shrudge/avie/archive/refs/tags/v1.0.3.tar.gz"
   sha256 "258e225ade31b29ce2c42d7fe20839f566fc83665676d4cb4700df045ac68f48"
   license "MIT"
 
