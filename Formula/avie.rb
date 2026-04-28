994316aa169094a0f19defbb1bc6514f982e6dd0
Meet Balani <74920401+shrudge@users.noreply.github.com>
2026-04-28 14:37:00 +0530

    feat: switch to instant binary distribution

--- Formula/avie.rb
+++ Formula/avie.rb
@@ -1,20 +1,20 @@
 class Avie < Formula
   desc "Swift package graph diagnostics tool"
   homepage "https://github.com/shrudge/avie"
-  url "https://github.com/shrudge/avie/archive/refs/tags/v1.0.3.tar.gz"
-  sha256 "e781021ff99aa73af32fe7dadaaa0b61b4dd1e08b41e2a2fc001ba628ad74df3"
+  
+
+  url "https://github.com/shrudge/avie/releases/download/v1.0.3/avie.tar.gz"
+  
+
+  sha256 "258e225ade31b29ce2c42d7fe20839f566fc83665676d4cb4700df045ac68f48"
+  
   license "MIT"
 
-  depends_on xcode: ["14.0", :build]
   depends_on :macos
 
   def install
-    inreplace "Sources/AvieCore/Version.swift", 
-              /public let avieToolVersion = .*/, 
-              "public let avieToolVersion = \"#{version}\""
 
-    system "swift", "build", "-c", "release", "--disable-sandbox"
-    bin.install ".build/release/avie"
+    bin.install "avie"
   end
 
   test do
