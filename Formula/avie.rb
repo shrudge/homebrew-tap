f7332115ac0cbc5c014a769632981fa618da5e4a
Meet Balani <74920401+shrudge@users.noreply.github.com>
2026-04-28 14:29:06 +0530

    fix: use validated download hash for v1.0.3

--- Formula/avie.rb
+++ Formula/avie.rb
@@ -2,7 +2,7 @@ class Avie < Formula
   desc "Swift package graph diagnostics tool"
   homepage "https://github.com/shrudge/avie"
   url "https://github.com/shrudge/avie/archive/refs/tags/v1.0.3.tar.gz"
-  sha256 "258e225ade31b29ce2c42d7fe20839f566fc83665676d4cb4700df045ac68f48"
+  sha256 "e781021ff99aa73af32fe7dadaaa0b61b4dd1e08b41e2a2fc001ba628ad74df3"
   license "MIT"
 
   depends_on xcode: ["14.0", :build]
