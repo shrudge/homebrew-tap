b3e36faa124ebdedca53d983a83da085298b9b03
Meet Balani <74920401+shrudge@users.noreply.github.com>
2026-04-30 18:56:20 +0530

    feat: release v1.0.5

--- Formula/avie.rb
+++ Formula/avie.rb
@@ -3,10 +3,10 @@ class Avie < Formula
   homepage "https://github.com/shrudge/avie"
   
 
-  url "https://github.com/shrudge/avie/releases/download/v1.0.4/avie.tar.gz"
+  url "https://github.com/shrudge/avie/releases/download/v1.0.5/avie.tar.gz"
   
 
-  sha256 "8530681465803e746576f795137614de99e4f908dd2ae98bc682ba11711b05fb"
+  sha256 "0852194f98d9fbbe85bc39b8d6698034aabd1773200ac8ea26dae16225697a76"
   
   license "MIT"
 
